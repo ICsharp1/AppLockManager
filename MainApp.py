81810d1f1f3c77e4d1e55d8ac6dd54d924402218
ICsharp1 <isracohe@g.jct.ac.il>
2022-06-08 16:49:31 +0300

    image

--- MainApp.py
+++ MainApp.py
@@ -5,15 +5,22 @@ from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+RESOURCEPATH = 'C:\\Users\\israe\\OneDrive\\שולחן העבודה\\\AppLockManager\\Resources\\'
+
 
 class MainApp(App):
     def build(self):
         label = Label(text='Hello from Kivy (#IC)',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+                      size_hint=(1, .5),
+                      pos_hint={'center_x': 0.18, 'center_y': 0.965})
+        label.font_size = '30sp'
 
-        return self.window
+        img = Image(source=RESOURCEPATH + 'KivyImage.jpg',
+                    size_hint=(1, .5),
+                    pos_hint={'center_x': .5, 'center_y': .5})
+        return label
 
 
 if __name__ == "__main__":
-    MainApp().run()
+    app = MainApp()
+    app.run()
